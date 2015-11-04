6a04a0d286920f461644e10d2db4f84f993b0990
Sindri Lin <634893985@qq.com>
2015-11-04 10:45:27 +0800

    Initial Commit

---
+++ LXDScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXDScrollView
+//
+//  Created by 林欣达 on 15/11/4.
+//  Copyright © 2015年 sindri lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXDScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXDScrollView
+//
+//  Created by 林欣达 on 15/11/4.
+//  Copyright © 2015年 sindri lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
